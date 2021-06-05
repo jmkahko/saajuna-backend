988cf21bade4cc13f9ab6feecb493cac0b1f3610
Leena Kähkönen <leenakahkonen14@gmail.com>
2021-06-05 15:32:32 +0300

    Lisätty uusi model saaennuste.js

---
+++ models/Saaennuste.js
@@ -0,0 +1,38 @@
+// Mongoose-skema, josta tehdään model joka exportataan
+const mongoose = require('mongoose');
+
+// Skeeman luonti. Skeema määrittää kannassa olevan tiedon muodon
+// Hirlam piste-ennuste 'simple feature'-muodossa
+const SaaennusteSchema = new mongoose.Schema({
+  time: { type: String }, // Kellonaika
+  GeopHeight: { type: Number },
+  Temperature: { type: Number },
+  Pressure: { type: Number },
+  Humidity: { type: Number },
+  WindDirection: { type: Number },
+  WindSpeedMS: { type: Number },
+  WindUMS: { type: Number },
+  WindVMS: { type: Number },
+  MaximumWind: { type: Number },
+  WindGust: { type: Number },
+  DewPoint: { type: Number },
+  TotalCloudCover: { type: Number },
+  WeatherSymbol3: { type: Number }, // Säätyypin kuvanumero
+  LowCloudCover: { type: Number },
+  MediumCloudCover: { type: Number },
+  HighCloudCover: { type: Number },
+  Precipitation1h: { type: Number },
+  PrecipitationAmount: { type: Number },
+  RadiationGlobalAccumulation: { type: Number },
+  RadiationLWAccumulation: { type: Number },
+  RadiationNetSurfaceLWAccumulation: { type: Number },
+  RadiationNetSurfaceSWAccumulation: { type: Number },
+  RadiationDiffuseAccumulation: { type: Number },
+  LandSeaMask: { type: Number },
+  place: { type: String }, // Paikannimi esim. Kajaani
+});
+
+// Tehdään skeemasta model, jonka metodeilla kantaoperaatioita suoritetaan. Model on luokka joka sisältää skeeman
+const Saaennuste = mongoose.model('Saaennuste', SaaennusteSchema); // Eka laitetaan modelin luokka ja mistä tulee
+
+module.exports = Saaennuste;
