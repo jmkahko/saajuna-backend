8b81c0c47a583323a8eaffbc89a6efce4cd6d213
Leena Kähkönen <leenakahkonen14@gmail.com>
2021-06-05 15:14:50 +0300

    Kommentti

--- models/Havaintoasema.js
+++ models/Havaintoasema.js
@@ -1,17 +1,17 @@
 // Mongoose-skema, josta tehdään model joka exportataan
 const mongoose = require('mongoose');
 
-// Skeeman luonti. Skeema määrittää kannassa olevan tiedon muodon
+// Havaintoaseman skeeman luonti. Skeema määrittää kannassa olevan tiedon muodon
 const HavaintoasemaSchema = new mongoose.Schema({
   name: { type: String }, // Havaintoaseman nimi
-  fmisid: { type: Number }, // FMISID
+  fmisid: { type: Number }, // FMISID, havaintoaseman yksilöllinen ID
   lpnn: { type: Number }, // LPNN
   wmo: { type: Number }, //  WMO
   longitude: { type: String, max: 9 }, // "25.006783"
   latitude: { type: String, max: 9 }, // "64.537118"
-  altitude: { type: Number }, // Korkeus
-  group: { type: String }, // Ryhmät
-  start: { type: String }, // Alkaen
+  altitude: { type: Number }, // Korkeus merenpinnasta
+  group: { type: String }, // Ryhmä
+  start: { type: String }, // Alkaen, milloin kyseinen havaintoasema on otettu käyttöön
 });
 
 // Tehdään skeemasta model, jonka metodeilla kantaoperaatioita suoritetaan. Model on luokka joka sisältää skeeman
