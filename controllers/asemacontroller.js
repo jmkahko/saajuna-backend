5d4dd3d5233eaa56108105966c40ee854f0dba8d
Janne Kähkönen <jm.kahkonen@gmail.com>
2021-06-23 16:31:58 +0300

    Lisätty JSON sanomaan rautatieasemien kokonimet

--- controllers/asemacontroller.js
+++ controllers/asemacontroller.js
@@ -99,94 +99,121 @@ const AsemaController = {
   // Hae aseman aikataulu esim. https://rata.digitraffic.fi/api/v1/live-trains/station/KAJ?arrived_trains=0&arriving_trains=5&departed_trains=0&departing_trains=2&include_nonstopping=false
   haeAsemanAikataulu: (req, res) => {
 
-    const station = req.params.station; // Asema
-    const arrived_trains = req.params.arrived_trains; // Kuinka monta saapunutta junaa palautetaan maksimissaan
-    const arriving_trains = req.params.arriving_trains; // Kuinka monta saapuvaa junaa palautetaan maksimissaan
-    const departed_trains = req.params.departed_trains; // Kuinka monta lähtenyttä junaa palautetaan maksimissaan
-    const departing_trains = req.params.departing_trains; // Kuinka monta lähtevää junaa palautetaan maksimissaan
-    // include_nonstopping=false suoraan on poistettu ne jotka ovat vaan läpikulussa, etteivät pysähdy
-
-    const url = 'https://rata.digitraffic.fi/api/v1/live-trains/station/' + station + '?arrived_trains=' + arrived_trains + '&arriving_trains=' + arriving_trains + '&departed_trains=' + departed_trains + '&departing_trains=' + departing_trains + '&include_nonstopping=false'
-
-    // Asetetaan haulle optioita. url sivu mistä haetaan, headers kerrotaan Accect-Encoding tieto, kun ilman sitä dataa ei saada
-    const options = {
-      url: url, // Laitetaan url tieto
-      headers: {
-        'accept-encoding': 'gzip', // Kerrotaan headerissa, että hyväksytään gzip encode
-      },
-      JSON: true, // JSON hyväksytään
-      method: 'GET', // Metodi on GET eli haku
-      gzip: true, // gzip hyväksytään. Ilman tätä riviä tieto on siansaksaa
-    };
-
-    request(options, function (error, response) {
-      // Jos tulee palautuksena arvo 200 niin tulostetaan saatu tulos
-      if (response.statusCode === 200) {
-        // Parseroidaan tulos data muuttujaan
-        let data = JSON.parse(response.body);
-
-        let asemantiedot = []; // Luodaan taulukko johon tallennetaan tiedot
-        let junientiedot = []; // Luodaan taulukko joohon tallennetaan junien tiedot
-
-        // Pyöräytetään ensimmäinen for silmukka kaikki junat jotka haku palauttaa
-        for (let i = 0; i < data.length; i++) {
-          junientiedot.push(['trainNumber', data[i]['trainNumber']]);
-          junientiedot.push(['trainType', data[i]['trainType']]); // Junatyyppi IC, Pendoliino ym.
-          junientiedot.push(['trainCategory', data[i]['trainCategory']]); // juna kategoria
-
-          // Asema jolta juna on lähtenyt
-          junientiedot.push(['startStation', data[i]['timeTableRows'][0]['stationShortCode']])
-
-          // Junan viimeinen asema
-          let timeTableRiveja = data[i]['timeTableRows'].length - 1;
-          junientiedot.push(['endStation', data[i]['timeTableRows'][timeTableRiveja]['stationShortCode']])
-          
-          // Halutun asemantiedot
-          junientiedot.push(['stationStop', station]);
-          // Jokainen juna erikseen for silmukassa
-          for (let x = 0; x < data[i]['timeTableRows'].length; x++) {
-            
-            // Haluttu asema
-            if (data[i]['timeTableRows'][x]['stationShortCode'] === station) {
-              // Saapuvan junan aikataulu UTC aika
-              if (data[i]['timeTableRows'][x]['type'] === 'ARRIVAL') {
-                junientiedot.push(['arrivalScheduledTime', data[i]['timeTableRows'][x]['scheduledTime']]);
-                junientiedot.push(['arrivalActualTime', data[i]['timeTableRows'][x]['actualTime']]);
-                junientiedot.push(['arrivalLiveEstimateTime', data[i]['timeTableRows'][x]['liveEstimateTime']]);
-                junientiedot.push(['arrivalDifferenceInMinutes', data[i]['timeTableRows'][x]['differenceInMinutes']]);
-              }
-
-              // Lähtevän junan aikataulu UTC aika
-              if (data[i]['timeTableRows'][x]['type'] === 'DEPARTURE') {
-                junientiedot.push(['departureScheduledTime', data[i]['timeTableRows'][x]['scheduledTime']]);           
-                junientiedot.push(['departureActualTime', data[i]['timeTableRows'][x]['actualTime']]);
-                junientiedot.push(['departureLiveEstimateTime', data[i]['timeTableRows'][x]['liveEstimateTime']]);
-                junientiedot.push(['departureDifferenceInMinutes', data[i]['timeTableRows'][x]['differenceInMinutes']]);
-              }
-            }
-          }
-
-          // Tehdään taulukosta objeckti
-          let arrayToObject = Object.fromEntries(new Map(junientiedot));  
-
-          // Tallennetaan junientiedon taulukko asemantiedot taulukkoon
-          asemantiedot.push(arrayToObject);
-          junientiedot = []; // Tyhjennetään taulukko
+    // Haetaan aluksi tietokannasta kaikki rautatieasemat
+    Asema.find((error, asemat) => {
+        // Jos tulee virhe niin lähetetään virhesanoma
+        if (error) {
+          throw error;
         }
+        
+        const station = req.params.station; // Asema
+        const arrived_trains = req.params.arrived_trains; // Kuinka monta saapunutta junaa palautetaan maksimissaan
+        const arriving_trains = req.params.arriving_trains; // Kuinka monta saapuvaa junaa palautetaan maksimissaan
+        const departed_trains = req.params.departed_trains; // Kuinka monta lähtenyttä junaa palautetaan maksimissaan
+        const departing_trains = req.params.departing_trains; // Kuinka monta lähtevää junaa palautetaan maksimissaan
+        // include_nonstopping=false suoraan on poistettu ne jotka ovat vaan läpikulussa, etteivät pysähdy
+    
+        const url = 'https://rata.digitraffic.fi/api/v1/live-trains/station/' + station + '?arrived_trains=' + arrived_trains + '&arriving_trains=' + arriving_trains + '&departed_trains=' + departed_trains + '&departing_trains=' + departing_trains + '&include_nonstopping=false'
+    
+        // Asetetaan haulle optioita. url sivu mistä haetaan, headers kerrotaan Accect-Encoding tieto, kun ilman sitä dataa ei saada
+        const options = {
+          url: url, // Laitetaan url tieto
+          headers: {
+            'accept-encoding': 'gzip', // Kerrotaan headerissa, että hyväksytään gzip encode
+          },
+          JSON: true, // JSON hyväksytään
+          method: 'GET', // Metodi on GET eli haku
+          gzip: true, // gzip hyväksytään. Ilman tätä riviä tieto on siansaksaa
+        };
+    
+        request(options, function (error, response) {
+          // Jos tulee palautuksena arvo 200 niin tulostetaan saatu tulos
+          if (response.statusCode === 200) {
+            // Parseroidaan tulos data muuttujaan
+            let data = JSON.parse(response.body);
+    
+            let asemantiedot = []; // Luodaan taulukko johon tallennetaan tiedot
+            let junientiedot = []; // Luodaan taulukko joohon tallennetaan junien tiedot
+    
+            // Pyöräytetään ensimmäinen for silmukka kaikki junat jotka haku palauttaa
+            for (let i = 0; i < data.length; i++) {
+              junientiedot.push(['trainNumber', data[i]['trainNumber']]);
+              junientiedot.push(['trainType', data[i]['trainType']]); // Junatyyppi IC, Pendoliino ym.
+              junientiedot.push(['trainCategory', data[i]['trainCategory']]); // juna kategoria
+    
+              // Asema jolta juna on lähtenyt
+              junientiedot.push(['startStation', data[i]['timeTableRows'][0]['stationShortCode']])
 
-        // Tulostetaan saatu tulos
-        console.log(asemantiedot);
+              // Junan viimeinen asema
+              let timeTableRiveja = data[i]['timeTableRows'].length - 1;
 
-        // Lähetetään JSON sanomana taulukko eteenpäin
-        res.json(asemantiedot);
+              // Käydään läpi tiedot, että saadaan asema-taulusta aseman kokonimi
+              for (let z = 0; z < asemat.length; z++) {
 
+                // Asemalta jolta juna on lähtenyt kokonimi
+                if (data[i]['timeTableRows'][0]['stationShortCode'] === asemat[z].stationShortCode) {
+                  junientiedot.push(['startStationLongName', asemat[z].stationName])
+                  startStationLongName = asemat[z].stationName;
+                }
 
-      } else {
-        // Jos tulee muu kuin 200 vastaus viestissä, niin tulostetaan koodi ja virhe viesti
-        console.log('Statuskoodi: ' + response.statusCode);
-        console.log('Virhe: ' + error);
+                // Junan viimeinen asema kokonimi
+                if (data[i]['timeTableRows'][timeTableRiveja]['stationShortCode'] === asemat[z].stationShortCode) {
+                  junientiedot.push(['endStationLongName', asemat[z].stationName])
+                  endStationLongName = asemat[z].stationName;
+                }
+              }
+
+              // Junan viimeinen asema
+              junientiedot.push(['endStation', data[i]['timeTableRows'][timeTableRiveja]['stationShortCode']])
+
+              // Halutun asemantiedot
+              junientiedot.push(['stationStop', station]);
+              // Jokainen juna erikseen for silmukassa
+              for (let x = 0; x < data[i]['timeTableRows'].length; x++) {
+                
+                // Haluttu asema
+                if (data[i]['timeTableRows'][x]['stationShortCode'] === station) {
+                  // Saapuvan junan aikataulu UTC aika
+                  if (data[i]['timeTableRows'][x]['type'] === 'ARRIVAL') {
+                    junientiedot.push(['arrivalScheduledTime', data[i]['timeTableRows'][x]['scheduledTime']]);
+                    junientiedot.push(['arrivalActualTime', data[i]['timeTableRows'][x]['actualTime']]);
+                    junientiedot.push(['arrivalLiveEstimateTime', data[i]['timeTableRows'][x]['liveEstimateTime']]);
+                    junientiedot.push(['arrivalDifferenceInMinutes', data[i]['timeTableRows'][x]['differenceInMinutes']]);
+                  }
+    
+                  // Lähtevän junan aikataulu UTC aika
+                  if (data[i]['timeTableRows'][x]['type'] === 'DEPARTURE') {
+                    junientiedot.push(['departureScheduledTime', data[i]['timeTableRows'][x]['scheduledTime']]);           
+                    junientiedot.push(['departureActualTime', data[i]['timeTableRows'][x]['actualTime']]);
+                    junientiedot.push(['departureLiveEstimateTime', data[i]['timeTableRows'][x]['liveEstimateTime']]);
+                    junientiedot.push(['departureDifferenceInMinutes', data[i]['timeTableRows'][x]['differenceInMinutes']]);
+                  }
+                }
+              }
+    
+              // Tehdään taulukosta objeckti
+              let arrayToObject = Object.fromEntries(new Map(junientiedot));  
+    
+              // Tallennetaan junientiedon taulukko asemantiedot taulukkoon
+              asemantiedot.push(arrayToObject);
+              junientiedot = []; // Tyhjennetään taulukko
+            }
+    
+            // Tulostetaan saatu tulos
+            console.log(asemantiedot);
+    
+            // Lähetetään JSON sanomana taulukko eteenpäin
+            res.json(asemantiedot);
+    
+    
+          } else {
+            // Jos tulee muu kuin 200 vastaus viestissä, niin tulostetaan koodi ja virhe viesti
+            console.log('Statuskoodi: ' + response.statusCode);
+            console.log('Virhe: ' + error);
+          }
+        });
       }
-    });
+    );
   },
 };
 
