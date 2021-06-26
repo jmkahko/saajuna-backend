3e6a73f03208f0629f1c8c1f6940443598613474
Leena Kähkönen <leenakahkonen14@gmail.com>
2021-06-26 16:20:47 +0300

    ennusteen haku latlon

--- controllers/havaintoasemacontroller.js
+++ controllers/havaintoasemacontroller.js
@@ -220,7 +220,7 @@ const HavaintoAsemaController = {
   },
 
   // Hae paikkakunnan sääennuste. Saadaan tunnin välein
-  haeAsemaSaaEnnuste: (req, response) => {
+  haeAsemaSaaEnnustePlace: (req, response) => {
     const place = req.params.place; // Saadaan paikkakunta
 
     let aika1 = new Date();
@@ -372,6 +372,194 @@ const HavaintoAsemaController = {
                       // Lähetetään viimeisin mittaustulos
                       Saaennuste.findOne(
                         { place: req.params.place },
+                        (error, saaennuste) => {
+                          // Jos tulee virhe niin lähetetään virhesanoma
+                          if (error) {
+                            throw error;
+                          }
+                          console.log('tuleeko tieto');
+                          response.json(saaennuste); // Lähetetään JSONina tietokannasta saatu tieto eteenpäin
+                        }
+                      ).sort({ _id: -1 });
+                    }
+                  }
+                });
+              });
+            }
+          });
+        }
+      }
+    ).sort({ _id: -1 });
+  },
+
+  // Hae paikkakunnan sääennuste koordinaateilla. Saadaan tunnin välein
+  haeAsemaSaaEnnuste: (req, response) => {
+    const place = req.params.place; // Saadaan paikkakunta
+    const lat = req.params.latitude; // Saadaan leveysasteen koordinaatit
+    const lon = req.params.longitude; // Saadaan pituusasteen koordinaatit
+
+    let aika1 = new Date();
+    aika1.setSeconds(0, 0); // Määritellään ajasta sekunnit ja millisekunnit nolliksi
+
+    // Ajan muunnoksia (vuosi, kuukausi, päivä, tunti ja minuutti). Sekunnit ja millisekunnit jätetään pois.
+    // FMI käyttää päivämäärä-tiedossaan UTC-aikaa, joten esimerkiksi tunneista pitää vähentää 3, jotta saadaan
+    // haku tapahtumaan oikeana aikana.
+    let vuosi = aika1.getFullYear();
+    let kuukausi = aika1.getMonth() + 1;
+    let paiva = aika1.getDate();
+    let tunti = aika1.getHours() - 3; // Ottaa tämä pois, kun siirtää Herokuuhun. Muuten ei toimi UTC aika
+
+    // Lisätään kuukausiin, päiviin, tunteihin ja minuutteihin 0 eteen mikäli ne ovat pienempiä kuin 10.
+    // Tällöin saadaan päivämäärätiedot oikeiksi ja määrämuotoisiksi hakua varten.
+    if (kuukausi < 10) {
+      kuukausi = '0' + kuukausi;
+    }
+
+    if (paiva < 10) {
+      paiva = '0' + paiva;
+    }
+
+    if (tunti < 10) {
+      tunti = '0' + tunti;
+    }
+
+    // Muodostetaan aika hakua varten määrämuotoisena
+    const aika =
+      vuosi + '-' + kuukausi + '-' + paiva + 'T' + tunti + ':' + '00';
+
+    Saaennuste.findOne(
+      { place: req.params.place },
+      { place: req.params.latitude },
+      { place: req.params.longitude },
+      { _id: false, time: true },
+      (error, kellonaika) => {
+        // Jos tulee virhe niin lähetetään virhesanoma
+        if (error) {
+          throw error;
+        }
+
+        // Lasketaan erotus millisekunteina
+        let erotus =
+          new Date(kellonaika.time).getTime() - new Date(aika).getTime();
+        let erotustunnit = erotus / 60000 / 60;
+
+        // Tulostetaan kuluva aika, tietokantaan tallennettu viimeisin, paljonko erotus ajoissa
+        console.log(aika1);
+        console.log(kellonaika.time);
+        console.log(erotustunnit);
+        console.log(erotus);
+
+        if (erotustunnit === 3) {
+          Saaennuste.findOne(
+            { place: req.params.place },
+            { place: req.params.latitude },
+            { place: req.params.longitude },
+            (error, saaennuste) => {
+              // Jos tulee virhe niin lähetetään virhesanoma
+              if (error) {
+                throw error;
+              }
+              response.json(saaennuste); // Lähetetään JSONina tietokannasta saatu tieto eteenpäin
+            }
+          ).sort({ _id: -1 });
+        } else {
+          // Viimeisimmästä säätiedon hausta on yli 10 minuuttia aikaa. Haetaan säätieto ja tallennetaan tietokantaan
+
+          const url =
+            //'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::timevaluepair&place=jaala&latlon=60.1,19.9&'
+            'https://opendata.fmi.fi/wfs?request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&starttime=' +
+            aika +
+            '&endtime=' +
+            aika +
+            '&place=' +
+            place +
+            '&latlon=' +
+            lat +
+            ',' +
+            lon;
+
+          console.log(url);
+          console.log(lat);
+          console.log(lon);
+          parser.on('error', function (err) {
+            console.log('Parser error', err);
+          });
+          // Luodaan taulukko, johon data haetaan
+          let data = '';
+          let taulukko = [];
+          https.get(url, function (res) {
+            if (res.statusCode >= 200 && res.statusCode < 400) {
+              res.on('data', function (data_) {
+                data += data_.toString();
+              });
+              res.on('end', function () {
+                // Parseroidaan saatu tulos result muuttujaan
+                parser.parseString(data, function (err, result) {
+                  // Tehdään try catch menettelyllä tietojen tallennus. Jos XML on tyhjä, niin ohjelman suoritus ei lakkaa kokonaan.
+
+                  try {
+                    // Lisätään kellonaika taulukkoon
+
+                    taulukko.push([
+                      'time',
+                      result['wfs:FeatureCollection']['wfs:member'][0][
+                        'BsWfs:BsWfsElement'
+                      ][0]['BsWfs:Time'].join(),
+                    ]);
+
+                    // Lisätään mittaus arvot taulukkoon
+                    for (let x = 0; x <= 23; x++) {
+                      // Haetaan parametrin nimi
+                      let parametrinimi =
+                        result['wfs:FeatureCollection']['wfs:member'][x][
+                          'BsWfs:BsWfsElement'
+                        ][0]['BsWfs:ParameterName'].join();
+
+                      // Haetaan mittaus tulos
+                      let parametritulos =
+                        result['wfs:FeatureCollection']['wfs:member'][x][
+                          'BsWfs:BsWfsElement'
+                        ][0]['BsWfs:ParameterValue'].join();
+
+                      // Muutetetaan saatu NaN tulos null arvoon, joka tallennetaan tietokantaan.
+                      if (parametritulos === 'NaN') {
+                        taulukko.push([parametrinimi, null]);
+                      } else {
+                        taulukko.push([parametrinimi, Number(parametritulos)]);
+                      }
+                    }
+
+                    // Lisätään paikkakunta taulukkoon
+                    taulukko.push(['place', place]);
+
+                    // Muutetaan taulukko objectiksi
+                    const arrayToObject = Object.fromEntries(new Map(taulukko));
+
+                    // Tulostetaan saatu tulos
+                    console.log(arrayToObject);
+
+                    // Tallennetaan saatu tulos tietokantaan
+                    const newSaaEnnuste = Saaennuste(arrayToObject);
+
+                    newSaaEnnuste.save(function (err) {
+                      if (err) {
+                        throw err;
+                      }
+                      console.log('Sääennuste tallennettu.');
+                    });
+                  } catch (e) {
+                    console.log('Tietojen haku epäonnistui');
+                    console.error(e.message);
+                  } finally {
+                    // Lisätään tiedon hakuun viive tallennuksen jälkeen, muuten tuloksena tulee vanha tallennus eikä nyt haettu tieto
+                    setTimeout(tallennuksenhaku, 1000);
+
+                    function tallennuksenhaku() {
+                      // Lähetetään viimeisin mittaustulos
+                      Saaennuste.findOne(
+                        { place: req.params.place },
+                        { place: req.params.latitude },
+                        { place: req.params.longitude },
                         (error, saaennuste) => {
                           // Jos tulee virhe niin lähetetään virhesanoma
                           if (error) {
