9cc9b2206face248c7c3e2471e4f4d033edd61bb
Janne Kähkönen <jm.kahkonen@gmail.com>
2021-07-10 17:57:17 +0300

    Heroku vienti

--- controllers/havaintoasemacontroller.js
+++ controllers/havaintoasemacontroller.js
@@ -72,7 +72,7 @@ const HavaintoAsemaController = {
         let paiva = aika1.getDate(); //Haetaan ajasta päivä-tieto.
 
         // Heroku / paikallisesti ajettava
-        let tunti = aika1.getHours() - 3; // Ota tämä -3 arvo pois, kun siirretään Herokuhun, koska muuten ei toimi UTC aika. Haetaan ajasta tunti-tieto.
+        let tunti = aika1.getHours(); // Ota tämä -3 arvo pois, kun siirretään Herokuhun, koska muuten ei toimi UTC aika. Haetaan ajasta tunti-tieto.
 
         let minuutti = aika1.getMinutes(); //Haetaan ajasta minuutti-tieto.
 
@@ -258,7 +258,7 @@ const HavaintoAsemaController = {
     let paiva = aika1.getDate();
 
     // Jos laitetaan Herokuhun, laitetaan tämä rivi kommenttiin
-    let tunti = aika1.getHours() - 3; // Laittaa -3 arvo kommenttiin, jos Herokussa, koska muuten ei toimi UTC aika.
+    let tunti = aika1.getHours(); // Laittaa -3 arvo kommenttiin, jos Herokussa, koska muuten ei toimi UTC aika.
 
     // Lisätään kuukausiin, päiviin, tunteihin ja minuutteihin 0 eteen mikäli ne ovat pienempiä kuin 10.
     // Tällöin saadaan päivämäärätiedot oikeiksi ja määrämuotoisiksi hakua varten.
